13a308c8b63434fa6630c839a949effa93b9e1cc
rachana-uniyal <rachanauniyal0417@gmail.com>
2021-10-08 18:30:25 +0530

    updated code with reselector

--- ecommerce-frontend/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ ecommerce-frontend/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import CustomButton  from "../custom-component/custom-button.component";
 import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss'
 import cartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selector";
 
 const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 )
 
-const mapStateToProps = ({ cart : { cartItems }}) => ({
-    cartItems
+const mapStateToProps = state => ({
+    cartItems:  selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
