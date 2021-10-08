91ce69432dcfc44866ba9123827c00a146c1b60a
rachana-uniyal <rachanauniyal0417@gmail.com>
2021-10-08 15:21:14 +0530

    added redirect links to homepage

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth,createUserProfileDocument } from './firebase/firebase.utils'
+import { auth,createUserProfileDocument, signInWithGoogle } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions';
 
 
@@ -47,19 +47,32 @@ class App extends React.Component {
       <Switch>
         <Route exact path ='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact 
+              path='/signin'
+              render={() => 
+                this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+              <SignInAndSignUpPage/>
+              )
+              }
+            />
       </Switch>
     </div>
   );
   }
 }
 
+const mapStateToProps = ({ user}) =>({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
   )(App);
  
